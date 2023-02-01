8486647d9f6fe42dcbc93eb2486a841bac2f2a07
dongjiacheng <dongjiacheng@corp.netease.com>
2023-02-01 15:50:19 +0800

    chore: 优化 cjs 模块导入

--- packages/vita-plugin-yapi/rollup.config.ts
+++ packages/vita-plugin-yapi/rollup.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     {
       dir: "dist",
       format: "cjs",
+      // 由于 node-fetch 混合了默认导出和具名导出
+      // 对 node-fetch 的默认导出进行 hack 处理
       interop: "auto",
       preserveModules: true,
       preserveModulesRoot: "src",
